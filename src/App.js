945107051014815c6cc3e75d549ee77072f8dffa
sethgonzales <sethgonzales157@gmail.com>
2023-12-03 20:45:40 -0800

    style about section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
-import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -12,7 +11,6 @@ const App = () => {
       <Home />
       <Projects />
       <About />
-      <Footer />
     </div>
   );
 };
--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import './About.css';
+import HeadShot from './../img/headshot.jpg';
 
 const About = () => {
   return (
     <div id="about" className="about">
-      <div className='about-left-column'>
-        <h1>It's Nice to Meet You</h1>
-      </div>
-      <div className='about-right-column'>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+      <div className="about-content">
+        <div className='about-left-column'>
+          <img alt='Seth Gonzales' src={HeadShot} className='headshot' />
+        </div>
+        <div className='about-right-column'>
+          <div className='welcome'>
+            <h1>It's Nice to Meet You</h1>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+          </p>
+          <div className='connection-links'>
+            <a href="" className='connection-link'>
+              <img src="email-logo.png" alt="Email" />
+            </a>
+            <a href="https://www.linkedin.com/in/seth-gonzales/" className='connection-link'>
+              <img src="linkedin-logo.png" alt="LinkedIn" />
+            </a>
+            <a href="https://github.com/sethgonzales" className='connection-link'>
+              <img src="github-logo.png" alt="GitHub" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
