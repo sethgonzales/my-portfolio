27f91103ebd1050eda66ee9babd235eabc090541
sethgonzales <sethgonzales157@gmail.com>
2023-12-11 15:20:04 -0800

    added code tags to projects and more animations

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -3,25 +3,31 @@ import './ProjectDetails.css';
 
 const ProjectDetails = ({ project }) => {
   return (
-      <div>
-        <div className="project-details-container">
-          <div className="project-details-left-column">
-            <img src={project.image} alt={project.title} />
-          </div>
-          <div className="project-details-right-column">
-            <h2>{project.title}</h2>
-            <p>{project.description}</p>
-            <div className='project-links'>
-              <a href={project.hostingURL} className='project-link'>
-                Open Application
-              </a>
-              <a href={project.github} className='project-link'>
-                GitHub
-              </a>
+    <div className='project-details-container'>
+      <div className='project-details-left-column'>
+        <img src={project.image} alt={project.title} />
+        <div className='code-used'>
+          {project.code.map((code, index) => (
+            <div key={index} className='code-tag'>
+              {code}
             </div>
-          </div>
+          ))}
+        </div>
+
+      </div>
+      <div className="project-details-right-column">
+        <h2>{project.title}</h2>
+        <p>{project.description}</p>
+        <div className='project-links'>
+          <a href={project.hostingURL} className='project-link'>
+            Open Application
+          </a>
+          <a href={project.github} className='project-link'>
+            GitHub
+          </a>
         </div>
       </div>
+    </div>
   );
 };
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,31 +1,32 @@
 import React, { useState } from 'react';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 import ProjectList from './ProjectList';
 import ProjectDetails from './ProjectDetails';
 import EOL from './../img/eol-chat.png'
 import ChemEdSimulations from './../img/chemed.png'
 import './Projects.css';
 
-
 const Projects = () => {
   const projects = [
-    // Your project data
     {
       id: 1,
       className: 'ChemEd',
       title: 'ChemEd Simulations',
       image: ChemEdSimulations,
-      description: 'ChemEd Simulations is an educational platform for students to access science related simulations. This is a full stack application using React, Firestore, JavaScript, CSS, and HTML',
+      description: 'Inspired by my time in the classroom, ChemEd is an educational platform for students to interact with simulations that explore scientific phenomenon.',
       github: 'https://github.com/sethgonzales/capstone', //update capstone github name
-      hostingURL: 'https://chemed-6a31a.web.app/'
+      hostingURL: 'https://chemed-6a31a.web.app/',
+      code: ["JS", "React", "Firestore", "CSS", "HTML"]
     },
     {
       id: 2,
       className: 'EOL',
       title: 'EOL - Epicodus Online',
       image: EOL,
-      description: "Epicodus Online is a recreation of AOL's Instant Messanger. This MVC application is built with Identity,  employing JavaScript for front-end logic and C# and SQL for back-end logic and database. SignalR is used for real-time messaging functionality in public and private chat rooms",
+      description: "Epicodus Online is a recreation of AOL's Instant Messenger. SignalR is used for real-time messaging functionality in public and private chat rooms.",
       github: 'https://github.com/sethgonzales/Eol.Solution',
-      hostingURL: '' //update this link
+      hostingURL: '', //update this
+      code: ["C#", "SignalR", "ASP.NET", "CSS", "HTML"]
 
     },
     // Add more projects...
@@ -42,11 +43,19 @@ const Projects = () => {
   return (
     <div id="projects" className="projects projects-container">
       <ProjectList projects={projects} onSelectProject={handleSelectProject} />
-      <div className='project-details'>
-        {selectedProject && (
-          <ProjectDetails project={selectedProject} />
-        )}
-      </div>
+      <SwitchTransition>
+        <CSSTransition
+          key={selectedProject.id}
+          classNames='fade'
+          timeout={500}
+        >
+          <div className='project-details'>
+            {selectedProject && (
+              <ProjectDetails project={selectedProject} />
+            )}
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
     </div>
   );
 };
