a4a86a56c0aa4209a49839155453078fcd4dc9c2
sethgonzales <sethgonzales157@gmail.com>
2023-12-10 16:16:54 -0800

    complete about me section info and styling

--- src/components/About.js
+++ src/components/About.js
@@ -8,24 +8,38 @@ const About = () => {
       <div className="about-content">
         <div className='about-left-column'>
           <img alt='Seth Gonzales' src={HeadShot} className='headshot' />
+          <div className='connection-links'>
+            <a href="mailto:sethgonzales157@gmail.com?subject=Hello%20Seth!" className='connection-link'>
+              <i className='fa fa-envelope'></i>
+            </a>
+            <a href="https://www.linkedin.com/in/seth-gonzales/" className='connection-link'>
+              <i className='fa fa-linkedin'></i>
+            </a>
+            <a href="https://github.com/sethgonzales" className='connection-link'>
+              <i className='fa fa-github'></i>
+            </a>
+          </div>
         </div>
         <div className='about-right-column'>
           <div className='welcome'>
             <h1>It's Nice to Meet You</h1>
           </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-          </p>
-          <div className='connection-links'>
-            <a href="" className='connection-link'>
-              <img src="email-logo.png" alt="Email" />
-            </a>
-            <a href="https://www.linkedin.com/in/seth-gonzales/" className='connection-link'>
-              <img src="linkedin-logo.png" alt="LinkedIn" />
-            </a>
-            <a href="https://github.com/sethgonzales" className='connection-link'>
-              <img src="github-logo.png" alt="GitHub" />
-            </a>
+          <div className='about-story'>
+            <p>
+              I am a former Chemistry Educator turned Software Developer.
+            </p><br />
+            <p>
+              In the classroom, I thrived on mentoring students and empowering them through inquiry-based education, all while occasionally showcasing the marvels of a highly exothermic, product favored reaction (aka <em>explosive</em> demonstrations).
+            </p><br />
+
+            <p>
+              Transitioning into software development, I bring a unique blend of communication, problem-solving, and leadership skills. I am eager to apply my expertise within a collaborative team; I aim to learn, contribute, and craft impactful solutions that resonate.
+            </p><br />
+
+            <p>
+              I would love the opportunity to grow within your team.
+            </p><br />
+
           </div>
         </div>
       </div>
